669e4cb4c276e22f8d6c9ba6d746858d2440a829
baxiang <baxiang1989@163.com>
2014-03-29 02:48:43 +0800

    update

--- OSChina/UserLoginViewController.m
+++ OSChina/UserLoginViewController.m
@@ -49,41 +49,51 @@
     [backBtn  setTitle:@"返回" forState:UIControlStateNormal];
     UIBarButtonItem *backItem= [[UIBarButtonItem alloc] initWithCustomView:backBtn];
     self.navigationItem.leftBarButtonItem = backItem;
-    UILabel *userName= [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 80, 44)];
-    userName.textAlignment = NSTextAlignmentLeft;
-    userName.font = [UIFont systemFontOfSize:18.0f];
-    userName.backgroundColor = [UIColor clearColor];
-    userName.text = @"  用户名: ";
-    //[self.view addSubview:userName];
-    userTextFiled= [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    UIButton *userName= [[UIButton alloc] initWithFrame:CGRectMake(0, 20, 100, 44)];
+    [userName setImage :[UIImage imageNamed:@"icon_myinfo_grey"] forState:UIControlStateNormal];
+    [userName setTitleEdgeInsets:UIEdgeInsetsMake(0, 0, -5, -10)];
+    [userName setTitle:@"用户名:" forState:UIControlStateNormal];
+    userName.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    [userName setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:userName];
+    userTextFiled= [[UITextField alloc] initWithFrame:CGRectMake(100, 20, 200, 44)];
+    userTextFiled.borderStyle = UITextBorderStyleNone;
     [self.view addSubview:userTextFiled];
-    userTextFiled.background = [UIImage imageNamed:@"bg_login_textfiled"];
-    userTextFiled.leftView = userName;
-    userTextFiled.leftViewMode = UITextFieldViewModeAlways;
-    UILabel *userPass= [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 80, 30)];
-    userPass.text = @"  密  码：";
-    userPass.font = [UIFont systemFontOfSize:18.0f];
-    passwordTextFiled= [[UITextField alloc] initWithFrame:CGRectMake(0, 50, 320, 44)];
-    passwordTextFiled.leftView = userPass;
-    passwordTextFiled.leftViewMode = UITextFieldViewModeAlways;
-    passwordTextFiled.background = [UIImage imageNamed:@"bg_login_textfiled"];
-    [self.view addSubview:passwordTextFiled];
     
+    UIImage *seprate= [UIImage imageNamed:@"login_seprateline"];
+    UIImageView *sepreLineOne = [[UIImageView alloc] initWithFrame:CGRectMake(20, userTextFiled.bottom, 260, 0.5)];
+    sepreLineOne.image = seprate;
+    [self.view addSubview:sepreLineOne];
+    UIButton *userPass= [[UIButton alloc] initWithFrame:CGRectMake(0, sepreLineOne.bottom+5, 100, 44)];
+    [userPass setImage :[UIImage imageNamed:@"icon_password_grey"] forState:UIControlStateNormal];
+    [userPass setTitleEdgeInsets:UIEdgeInsetsMake(0, 0, -5, -10)];
+    [userPass setTitle:@"密  码:" forState:UIControlStateNormal];
+    userPass.titleLabel.font = [UIFont systemFontOfSize:16.0];
+    [userPass setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.view addSubview:userPass];
+    passwordTextFiled= [[UITextField alloc] initWithFrame:CGRectMake(100, sepreLineOne.bottom+5, 200, 44)];
+    passwordTextFiled.borderStyle = UITextBorderStyleNone;
+    [self.view addSubview:passwordTextFiled];
+    UIImageView *sepreLineTwo = [[UIImageView alloc] initWithFrame:CGRectMake(20, passwordTextFiled.bottom, 260, 0.5)];
+    sepreLineTwo.image = seprate;
+    [self.view addSubview:sepreLineTwo];
     UIButton *loginBtn= [UIButton buttonWithType:UIButtonTypeCustom];
-    loginBtn.frame = CGRectMake(0, passwordTextFiled.bottom+30, 320, 50);
-    [loginBtn setBackgroundImage:[UIImage imageNamed:@"Navibarbg"] forState:UIControlStateNormal];
+    loginBtn.frame = CGRectMake(25, passwordTextFiled.bottom+30, 270, 45);
+    [loginBtn setBackgroundImage:[[UIImage imageNamed:@"btn_login_blue"] stretchableImageWithLeftCapWidth:20 topCapHeight:20]forState:UIControlStateNormal];
     [loginBtn addTarget:self action:@selector(userLoginHandle) forControlEvents:UIControlEventTouchUpInside];
     [loginBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [loginBtn setTitle:@"登     录" forState:UIControlStateNormal];
+    [loginBtn setTitle:@"登    录" forState:UIControlStateNormal];
     [self.view addSubview:loginBtn];
     
 }
 -(void) userLoginHandle{
     __weak UserLoginViewController *weakSelf = self;
+    userTextFiled.text = @"baxiang1989@163.com";
+    passwordTextFiled.text  = @"yangyu1989";
      [[OSAPIClient shareClient] userLoginName:userTextFiled.text passWord:passwordTextFiled.text RequestResult:^(id resultDatas, NSError *error) {
          if ([resultDatas isKindOfClass:[NSString class]]&&[resultDatas isEqualToString:@"1"]) {
                 UserInfoViewController  *userInfo = [[UserInfoViewController alloc] init];
-             [self.navigationController pushViewController:userInfo animated:NO];
+             [weakSelf.navigationController pushViewController:userInfo animated:NO];
 //               weakSelf.LoginState(YES);
 //              [weakSelf.navigationController popViewControllerAnimated:NO];
          }else{
